8f8b6efa06447b6b909b03ab479e08071796c9ad
Pratikk2op <pratikpgore2001@gmail.com>
2025-09-10 01:58:12 +0530

    url added cors config

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
   ? [process.env.FRONTEND_URL]
   : [
       'http://localhost:5173',
-      'mechiee.netlify.app',
+      'https://mechiee.netlify.app/',
       'http://localhost:5174',
       'http://localhost:5175',
       'http://localhost:5176',
