1bb4e58a70b1c0281feb7657a20644f3cbc652ca
Pratikk2op <pratikpgore2001@gmail.com>
2025-09-10 01:37:34 +0530

    url added cors config

--- server/server.js
+++ server/server.js
@@ -80,12 +80,14 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
   ? [process.env.FRONTEND_URL]
   : [
       'http://localhost:5173',
+      'mechiee.netlify.app',
       'http://localhost:5174',
       'http://localhost:5175',
       'http://localhost:5176',
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://backend-3lsi.onrender.com'
+      'https://mechiee.onrender.com',
+      ''
     ];
 
 const corsOptions = {
